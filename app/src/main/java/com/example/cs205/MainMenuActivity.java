e511a09825416d200260b3de5d3f622601b2a6db
NgJackin <ngjackin2016@gmail.com>
2025-04-14 23:39:12 +0800

    fix menu page

--- app/src/main/java/com/example/cs205/MainMenuActivity.java
+++ app/src/main/java/com/example/cs205/MainMenuActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -23,5 +25,25 @@ public class MainMenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        
+        Button howToPlayButton = findViewById(R.id.howToPlayButton);
+        howToPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHowToPlayDialog();
+            }
+        });
+    }
+    
+    private void showHowToPlayDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("How To Play")
+                .setMessage("1. Drag and drop process blocks onto the CPU grid\n\n" +
+                        "2. Each block takes time to process - watch the progress!\n\n" +
+                        "3. Fill entire rows to complete them and earn points\n\n" +
+                        "4. Don't let your waiting processes starve (turn red)\n\n" +
+                        "5. Keep managing processes efficiently as long as possible!")
+                .setPositiveButton("Got it!", null)
+                .show();
     }
 }
