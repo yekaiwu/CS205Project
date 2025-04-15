746efb8628fb56b43c0b32a6c35de555a930be19
ju57_4_8361nn3r <xuan2103yyx@gmail.com>
2025-04-16 02:41:08 +0800

    feat: added process counter and timer

--- app/src/main/java/com/example/cs205/GameActivity.java
+++ app/src/main/java/com/example/cs205/GameActivity.java
@@ -1,16 +1,23 @@
 package com.example.cs205;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
 /**
  * A class representing the game activity.
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements Timer.TimerListener {
 
     private GameView gameView;
+    private TextView timerTextView;
+    private Timer timer;
+    private HighScoreDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,11 @@ public class GameActivity extends AppCompatActivity {
         
         setContentView(R.layout.activity_game);
         
-        gameView = findViewById(R.id.gameView);
+        gameView = findViewById(R.id.gameView); // get game view
+
+        timerTextView = findViewById(R.id.timerTextView); // get timer text view
+        timer = new Timer(20000, 1000, this); // 1 minute timer with 1 second interval
+        dbHelper = new HighScoreDatabaseHelper(this);
         
         // Find and configure reset overflow button
         Button resetButton = findViewById(R.id.reset_overflow_button);
@@ -36,6 +47,11 @@ public class GameActivity extends AppCompatActivity {
                     }
                 }
             });
+        }
+
+        if (!timer.isRunning()) {
+            Log.d("GameActivity", "Timer started");
+            timer.start();
         }
     }
 
@@ -53,5 +69,31 @@ public class GameActivity extends AppCompatActivity {
         if (gameView != null) {
             gameView.resume();
         }
+    }
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+        long minutes = (millisUntilFinished / 1000) / 60;
+        long seconds = (millisUntilFinished / 1000) % 60;
+        String timeFormatted = String.format("%02d:%02d", minutes, seconds);
+        timerTextView.setText("Time left: " + timeFormatted);
+    }
+
+    @Override
+    public void onFinish() {
+        Log.d("GameActivity", "Timer finished");
+        timerTextView.setText("Time's up!");
+        int score = gameView.endGame();
+        saveHighestCounter(score);
+        // push notification
+        NotificationPublisher.showNotification(this);
+        // should display score stats page, now it just go back to main activity page
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void saveHighestCounter(int score) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        dbHelper.saveHighestScore(db, score);
     }
 } 
--- app/src/main/java/com/example/cs205/MainActivity.java
+++ app/src/main/java/com/example/cs205/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.TextView;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.Manifest;
 /**
  * A class representing the main activity which contains a button to start the game.
  */
@@ -16,8 +21,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // display highest score saved in local db
+        HighScoreDatabaseHelper dbHelper = new HighScoreDatabaseHelper(this);
+        TextView highScoreTextView = findViewById(R.id.highScoreTextView);
+
+        // clear db for testing, comment out when not needed
+        // run the app once w this code, then stop the app and comment out this code then run again
+//         SQLiteDatabase dbTest = dbHelper.getWritableDatabase();
+//         dbHelper.clearScores(dbTest);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        int highScore = dbHelper.getHighestScore(db);
+        highScoreTextView.setText("High Score: " + highScore);
+
         // Find the start game button
         final Button startButton = findViewById(R.id.start_button);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        }
         
         // Set a listener to start the game on click
         startButton.setOnClickListener(new View.OnClickListener() {
